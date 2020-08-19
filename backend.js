2c87edcad5bd9943f58795589d877109ccfe491f
mkotsollaris <mkotsollari@gmail.com>
2020-08-19 10:49:27 -0700

    added example keys

--- backend.js
+++ backend.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 3010;
 
 const truliooMiddleware = require('trulioo-embedid-middleware')({
+  // NOTE: Commiting this key for example-purposes only. You should never commit this key; instead load it as a secret.
   apiKey: '' // <-- put your API KEY here (ideally this value is added through as an environment variable i.e.: process.env.TRULIOO_API_KEY)
 });
 
